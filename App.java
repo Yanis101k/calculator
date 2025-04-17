34413dbb7169f299f2c8d19a487b7da1cc9e0306
Yanis101K <yaniskaced41@gmail.com>
2025-04-17 20:06:27 +0100

    intial setup : project structure created

---
+++ App.java
@@ -0,0 +1,6 @@
+
+// this class will be starting point of our application will includes menu's and calling Calculator class methodes 
+
+public class App {
+    
+}
---
+++ Calculator.java
@@ -0,0 +1,5 @@
+// Calculator class that will conataine methodes that implement basic calculator methodes like : + , - , * , / , % 
+
+public class  Calculator {
+    
+}
