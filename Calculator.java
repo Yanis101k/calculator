81cbbcc57fc04f849e4983c50aa799c62c265a9d
Yanis101K <yaniskaced41@gmail.com>
2025-04-17 20:43:18 +0100

    feat : implement substraction methode

--- Calculator.java
+++ Calculator.java
@@ -4,17 +4,27 @@ public class  Calculator {
 
     // methode that will return the addtion result of two number 
 
-    public int addition( int a , int b ){
+    public double addition( double a ,  double b ){
 
         return a + b ; 
 
     }
+
+    
+    // methode that will return the addtion result of two number 
+
+    public double substraction( double a , double b ){
+        return a - b ; 
+    }
+
 
     // main methode to teste manually the e=above methodes 
     public static void main( String args[] ){
         Calculator calculator = new Calculator() ; 
 
         System.out.println(calculator.addition(4, -3));
+
+        System.out.println( calculator.substraction(5.9, 9.12 ));
     }
 
 }
