7a68aaebba7dc983805a948333661c60ed766ac8
Yanis101K <yaniskaced41@gmail.com>
2025-04-17 21:03:12 +0100

    feat : implement multiplication methods

--- Calculator.java
+++ Calculator.java
@@ -11,20 +11,26 @@ public class  Calculator {
     }
 
     
-    // methode that will return the addtion result of two number 
+    // methode that will return the  result of two number 
 
     public double substraction( double a , double b ){
         return a - b ; 
     }
 
+    // methode that will return the multiplication reslt of two number  
+    public double multiplication( double a , double b ){
+        return a * b ; 
+    } 
 
-    // main methode to teste manually the e=above methodes 
+    // main methode to teste manually the above methodes 
     public static void main( String args[] ){
         Calculator calculator = new Calculator() ; 
 
         System.out.println(calculator.addition(4, -3));
 
         System.out.println( calculator.substraction(5.9, 9.12 ));
+
+        System.out.println( calculator.multiplication(0, 2.2));
     }
 
 }
