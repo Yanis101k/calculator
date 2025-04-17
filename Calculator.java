e04f0018222150198db85d54209e39219397ae84
Yanis101K <yaniskaced41@gmail.com>
2025-04-17 20:15:05 +0100

    feat : implementing addition feature and test it

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,20 @@
 // Calculator class that will conataine methodes that implement basic calculator methodes like : + , - , * , / , % 
 
 public class  Calculator {
-    
+
+    // methode that will return the addtion result of two number 
+
+    public int addition( int a , int b ){
+
+        return a + b ; 
+
+    }
+
+    // main methode to teste manually the e=above methodes 
+    public static void main( String args[] ){
+        Calculator calculator = new Calculator() ; 
+
+        System.out.println(calculator.addition(4, -3));
+    }
+
 }
